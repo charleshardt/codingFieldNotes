e22f8f529caf3232e611c54b93b971b40cda47b8
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-02-10 10:59:13 -0500

    Update codewars.js
    Added exclamation mark kata

--- JavaScript/codewars.js
+++ JavaScript/codewars.js
@@ -104,4 +104,22 @@ function getAge(inputString) {
 console.log(getAge("2 years old."));
 //////////////////////////////////////////////////////////////////////////
 
+/**
+ * 8 kyu: Exclamation marks series #1:  
+ * Remove an exclamation mark from the end of string
+ *
+ * Kata: Remove an exclamation mark from the end of a string. For a 
+ * beginner kata, you can assume that the input data is always a string, 
+ * no need to verify it.
+ * 
+ */
+
+const remove = (string) => (string.endsWith("!")) ? string.slice(0, string.length - 1) : string;
+
+console.log(remove("Hello!"));
+console.log(remove("Hi!"));
+console.log(remove("!Hi"));
+//////////////////////////////////////////////////////////////////////////
+
+
 
