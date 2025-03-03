6a2bf1791fabf8e9ea1f47e642d8174143d46088
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-03-03 15:16:31 -0500

    Add files via upload

---
+++ JavaScript/The Complete JavaScript Course/script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+let upperLimit = document.querySelector('.upper-limit').value;
+const randomNumber = () => Math.trunc(Math.random() * upperLimit) + 1;
+let secretNumber = randomNumber();
+
+// Allow users to change upper limit.
+document.querySelector('.upper-limit').addEventListener('change', function () {
+  upperLimit = this.value;
+});
+
+let score = 20; // score is a state variable
+let highScore = 0;
+
+const theMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const theGuess = function (guess) {
+  document.querySelector('.guess').value = guess;
+};
+
+document.querySelector('.highscore').textContent = highScore;
+document.querySelector('.score').textContent = score;
+document.querySelector('.number').textContent = '?';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // When there is no input.
+  if (!guess) {
+    theMessage('⛔ Gee whiz, write a number, yo...');
+
+    // When guess is right
+  } else if (guess === secretNumber) {
+    // Player wins
+    document.querySelector('.number').textContent = secretNumber;
+    theMessage('🔥 NAILED it! 🔥');
+
+    // Check if this is the highest score to date
+    if (score > highScore) highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+    document.querySelector('body').style.backgroundColor = '#171717 ';
+    document.querySelector('.number').style.border = '20px #FCE100 solid';
+    document.querySelector('.message').style.fontSize = '3rem';
+    document.querySelector('.number').style.width = '40rem';
+    document.querySelector('.number').style.color = '#F7630C ';
+    document.querySelector('.number').style.border = '15px #FCE100solid';
+
+    // When guess is wrong
+  } else {
+    if (score > 1) {
+      guess > secretNumber
+        ? theMessage("👇 You're too HIGH")
+        : theMessage("👆 You're too LOW");
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.score').textContent = 0;
+      theMessage('You have died of Dysentery.');
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  // Reset values
+  score = 20; // score is a state variable
+  secretNumber = randomNumber();
+
+  // Reset message, score, number, and guess
+  theMessage('Start guessing, Bucko...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  theGuess('');
+
+  // Reset body background color
+  document.querySelector('body').style.backgroundColor = '#111';
+  document.querySelector('.number').style.border = '20px #333 solid';
+  document.querySelector('.number').style.width = '20rem';
+  document.querySelector('.number').style.color = '#333 ';
+});
