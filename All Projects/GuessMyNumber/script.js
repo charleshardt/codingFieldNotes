d02b2d9784d708457eab75c92c40abff073b80a3
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-03-05 15:51:21 -0500

    Update script.js

--- All Projects/GuessMyNumber/script.js
+++ All Projects/GuessMyNumber/script.js
@@ -1,106 +1,150 @@
 'use strict';
 
+// Variables
 let upperLimit = document.querySelector('.upper-limit').value;
 const randomNumber = () => Math.trunc(Math.random() * upperLimit) + 1;
 let secretNumber = randomNumber();
-
-// Variables
 let score = 20;
 let highScore = 0;
 
-const theMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+const upperLimitBox = document.querySelector('.upper-limit');
+const bigMessageBox = document.querySelector('h1');
+const numberBox = document.querySelector('.number');
+const scoreCard = document.querySelector('.score');
+const highScoreCard = document.querySelector('.highscore');
+const highScoreBoard = document.querySelector('.label-highscore');
+const guessBox = document.querySelector('.guess');
+const messageBox = document.querySelector('.message');
+const overlay = document.querySelector('.overlay');
+const submitButton = document.querySelector('.enter');
+const againButton = document.querySelector('.again');
+
+const displayMessage = function (message) {
+  messageBox.textContent = message;
 };
-const theGuess = function (guess) {
+
+const displayGuess = function (guess) {
   document.querySelector('.guess').value = guess;
 };
 
-document.querySelector('.highscore').textContent = highScore;
-document.querySelector('.score').textContent = score;
-document.querySelector('.number').textContent = '?';
+highScoreCard.textContent = highScore;
+scoreCard.textContent = score;
+numberBox.textContent = '?';
 
-// Allow users to change upper limit.
-document.querySelector('.upper-limit').addEventListener('change', function () {
-  upperLimit = this.value;
+// Some Basic Functionality
+
+// Key press "Enter" from inside the guess box
+guessBox.addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') submitButton.click();
 });
 
-document.querySelector('.enter').addEventListener('click', function () {
+// Allow users to change upper limit.
+upperLimitBox.addEventListener('change', function () {
+  upperLimit = this.value;
+  guessBox.setAttribute('max', this.value);
+  againButton.click();
+});
+
+// Game Play: Player clicks the submit button.
+submitButton.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  // When there is no input.
+  // Guess box is empty
   if (!guess) {
-    theMessage('⛔ Gee whiz, enter a number, yo...');
+    nudge(guessBox);
+    messageBox.textContent = '⛔ Gee whiz, enter a number, yo...';
 
-    // When guess is right
+    // Player WINS.
   } else if (guess === secretNumber) {
-    // Player wins
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('h1').textContent = '🔥 NAILED it! 🔥';
-    theMessage('Bravo, Svengali.');
+    numberBox.textContent = secretNumber;
+    bigMessageBox.textContent = '🔥 NAILED it! 🔥';
+    displayMessage('Bravo, Svengali.');
 
-    // Check if this is the highest score to date
-    if (score > highScore) highScore = score;
-    document.querySelector('body').style.backgroundColor = '#171717 ';
-    document.querySelector('.guess').style.border = '4px #FCE100 solid';
-    document.querySelector('.highscore').textContent = highScore;
+    // Change the styles.
+    guessBox.style.border = '4px #FCE100 solid';
+    messageBox.style.fontSize = '3rem';
+    numberBox.style.border = '20px #FCE100 solid';
+    numberBox.style.width = '40rem';
+    numberBox.style.color = '#F7630C ';
+    numberBox.style.border = '15px #FCE100solid';
+    againButton.focus();
 
-    document.querySelector('.label-highscore').classList.add('blink');
-    document.querySelector('.message').style.fontSize = '3rem';
-    document.querySelector('.number').style.border = '20px #FCE100 solid';
-    document.querySelector('.number').style.width = '40rem';
-    document.querySelector('.number').style.color = '#F7630C ';
-    document.querySelector('.number').style.border = '15px #FCE100solid';
+    // Is it a high score?
+    if (score > highScore) {
+      highScore = score;
+      highScoreBoard.classList.add('blink');
+      highScoreCard.textContent = highScore;
+    }
 
-    // When guess is wrong
+    // Player guesses wrong.
   } else {
     if (score > 1) {
       guess > secretNumber
-        ? theMessage("👇 You're too HIGH")
-        : theMessage("👆 You're too LOW");
+        ? displayMessage("👇 You're too HIGH")
+        : displayMessage("👆 You're too LOW");
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreCard.textContent = score;
+
+      // Player LOSES.
     } else {
-      document.querySelector('h1').textContent =
-        'You have died of Dysentery, Yo';
-      document.querySelector('.again').style.border = '6px red solid';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('.enter').style.backgroundColor = '#333';
-      document.querySelector('.enter').disabled = true;
-      document.querySelector('.overlay').classList.remove('hidden');
+      scoreCard.textContent = 0;
+      submitButton.disabled = true;
+      overlay.classList.remove('hidden');
+      bigMessageBox.textContent = 'You have died of Dysentery, Yo';
+      againButton.style.border = '6px red solid';
+      submitButton.style.backgroundColor = '#333';
+      againButton.focus();
     }
   }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
+// The "Again!" button resets everything.
+againButton.addEventListener('click', function () {
   // Reset values
-  score = 20; // score is a state variable
+  score = 20;
   secretNumber = randomNumber();
 
-  // Reset message, score, number, and guess
-  theMessage('Start guessing, Bucko...');
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  theGuess('');
+  // Reset score, message box, number box, and guess box
+  scoreCard.textContent = score;
+  displayMessage('Start guessing, Bucko...');
+  numberBox.textContent = '?';
+  guessBox.value = '';
 
-  // Reset body background color
-  document.querySelector('h1').textContent = 'Guess My Number, Yo';
-  document.querySelector('.again').style.border = '4px #eee solid';
-  document.querySelector('.again').focus(); //why doesn't this work?
-  document.querySelector('.overlay').classList.add('hidden');
-  document.querySelector('body').style.backgroundColor = '#111';
-  document.querySelector('.number').style.border = '20px #333 solid';
-  document.querySelector('.guess').style.border = '4px #eee solid';
-  document.querySelector('.number').style.width = '20rem';
-  document.querySelector('.number').style.color = '#333 ';
-  document.querySelector('.enter').style.backgroundColor = '#ccc';
-  document.querySelector('.enter').disabled = false;
-  document.querySelector('.label-highscore').classList.remove('blink');
+  // Reset styles
+  againButton.style.border = '4px #eee solid';
+  submitButton.style.backgroundColor = '#ccc';
+  submitButton.disabled = false;
+  guessBox.style.border = '4px #eee solid';
+  bigMessageBox.textContent = 'Guess My Number, Yo';
+  numberBox.style.border = '20px #333 solid';
+  numberBox.style.width = '20rem';
+  numberBox.style.color = '#333 ';
+  overlay.classList.add('hidden');
+  highScoreBoard.classList.remove('blink');
+  // guessBox.focus(); // not working. triggers enter button.
+  // guessBox.select();
 });
 
 // Animations
 
+// Nudge an element
+const nudge = function (element) {
+  element.animate(
+    [
+      // keyframes
+      { transform: 'rotate(-4deg)' },
+      { transform: 'rotate(4deg)' },
+      { transform: 'rotate(-4deg)' },
+    ],
+    {
+      // timing
+      duration: 250,
+    }
+  );
+};
+
 // Click Enter button
-document.querySelector('.enter').addEventListener('click', function () {
+submitButton.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   // Spin if guess box is not empty
