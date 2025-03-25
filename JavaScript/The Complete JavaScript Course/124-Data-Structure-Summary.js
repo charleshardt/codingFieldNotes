93d954ce645fde9b982eb323daa3905139e757e7
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-03-25 17:27:18 -0400

    Create 124-Data-Structure-Summary.js

---
+++ JavaScript/The Complete JavaScript Course/124-Data-Structure-Summary.js
@@ -0,0 +1,35 @@
+/*
+////////////////////////////////////////////////////////
+// Lesson 124. Data Structure Summary
+
+Sources of Data:
+1) Source code
+2) The UI (DOM)
+3) web API
+
+Received in collections, stored in data structures.
+
+- [ ] Simple List? Array or Set
+- [ ] Key=>Value: Object or Map
+
+Use:
+  
+Arrays
+  ...ordered list of values, potential duplicates.
+  ...have to manipulate data.
+Sets
+  ...unique values
+  ...high-performance is important
+  ...use to remove duplicates
+Objects
+  ...easy to write and access values with . and []
+  ...most developers already familiar with objects
+  ...you need functions as values (methods) and need <this>
+  ...when working with JSON
+Maps
+  ...better for simple key=>value storage
+  ...better performance
+  ...keys: any data type (not strings)
+  ...easy to iterate
+  ...easy to compute size
+*/
