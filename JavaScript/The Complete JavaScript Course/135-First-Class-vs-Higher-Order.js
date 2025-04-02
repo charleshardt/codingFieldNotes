1a36bbcfa915233cdee754879cd404bd814f6b55
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-04-01 22:18:11 -0400

    Create 135-First-Class-vs-Higher-Order.js

---
+++ JavaScript/The Complete JavaScript Course/135-First-Class-vs-Higher-Order.js
@@ -0,0 +1,7 @@
+///////////////////////////////////////
+// Lesson 135. First-Class vs Higher-Order Functions
+// A subtle difference
+
+// A 'first-class function' is one that is stored as a value.
+
+// A 'higher-order function' receives another function as an argument and/or returns another function. A language that supports first-class functions is able to have higher-order functions.
