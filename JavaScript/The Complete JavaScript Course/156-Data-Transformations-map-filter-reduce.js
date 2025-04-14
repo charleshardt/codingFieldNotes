953e91bec6e7ff7577638c83fab44aaf691c35b8
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-04-14 13:14:43 -0400

    Create 156-Data-Transformations-map-filter-reduce.js

---
+++ JavaScript/The Complete JavaScript Course/156-Data-Transformations-map-filter-reduce.js
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////
+// 156-Data-Transformations-map-filter-reduce
+
+// map() applies a specified operation to each element. It is more useful than forEach because it returns a new array
+
+// filter() returns a new array containing the elements that returned true for a test condition
+
+// reduce() uses an accumulator variable to reduce all array elements down to a single value and returns that value
+
