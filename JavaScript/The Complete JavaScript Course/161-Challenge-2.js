76963bbd18db46b7fbc8405dd11718b4cc619d7e
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-04-21 16:28:42 -0400

    Update 161-Challenge-2.js

--- JavaScript/The Complete JavaScript Course/161-Challenge-2.js
+++ JavaScript/The Complete JavaScript Course/161-Challenge-2.js
@@ -1,10 +1,34 @@
+///////////////////////////////////////
+// 161-Challenge-2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// Thanks you but no luck needed:
+
 const ages = [5, 2, 4, 1, 15, 8, 3];
 // const ages = [16, 6, 10, 5, 6, 1, 4];
-console.log(`In doge years:`, ages);
+console.log(`I gots these in doge years:`, ages);
 
 const calcAverageHumanAge = function (ages) {
-  const agesHuman = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
-  console.log(`In hooman years:`, agesHuman);
+  const agesHuman = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(`Now in hooman years:`, agesHuman);
 
   const dogsOver18 = agesHuman.filter(function (humanAge) {
     return humanAge > 18;
@@ -14,7 +38,7 @@ const calcAverageHumanAge = function (ages) {
   const avgAdultAge =
     dogsOver18.reduce((acc, cur) => acc + cur, 0) / dogsOver18.length;
   console.log(`Avg hooman age of adult doges:`, avgAdultAge);
-  console.log(`
+  console.log(`lol
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠋⠈⠙⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⢤⡀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠈⢇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠞⠀⠀⢠⡜⣦⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡃⠀⠀⠀⠀⠈⢷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⣠⠀⠀⠀⠀⢻⡘⡇
