2b349994e36f6e6c37af8981ad01fd0eb6dc1ef7
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-05-07 20:17:27 -0400

    Create 9-Values-Variables.js

---
+++ JavaScript/The Complete JavaScript Course/9-Values-Variables.js
@@ -0,0 +1,12 @@
+///////////////////////////////////////
+// 9-Values-Variables
+
+const country = 'USA';
+const continent = 'North America';
+let population = 130;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(typeof null); // this is a bug in javascript which has never been corrected for legacy reasons.
