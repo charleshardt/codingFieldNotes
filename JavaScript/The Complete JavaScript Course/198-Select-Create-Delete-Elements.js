bfd4caa39be75e2a7caaa4264e3ff44e8ec2ee1e
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-05-13 17:55:12 -0400

    Update 198-Select-Create-Delete-Elements.js
    Finished the lesson

--- JavaScript/The Complete JavaScript Course/198-Select-Create-Delete-Elements.js
+++ JavaScript/The Complete JavaScript Course/198-Select-Create-Delete-Elements.js
@@ -9,9 +9,10 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-// the most used ways to select elements
+// most common ways to select elements
+
 // available on the document and on the elements
-document.querySelector('.header');
+const header = document.querySelector('.header');
 
 const allSections = document.querySelectorAll('.section');
 console.log(allSections); // returns a node list
@@ -23,3 +24,44 @@ console.log(allButtons); // returns an HTMLCollection
 // an HTMLCollection is a "live" collection, because if the DOM changes, the collection is immediately updated automatically. Useful when writing programmaticaly instead of manually.
 
 console.log(document.getElementsByClassName('btn')); // returns a live HTMLCollection
+
+// HOW TO CREATE AND INSERT ELEMENTS
+// Quick and easy way: .insertAdjacentHTML;
+
+// Build more programmatically:
+
+const message = document.createElement('div');
+// It's not in the DOM yet
+
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookies for enhanced functionality and analytics.';
+message.innerHTML =
+  'We use cookies for enhanced functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message); // moved to first position as child element
+header.append(message); // moved to last position as child element
+
+// message is a live HTMLCollection object, so it can't be in 2 places at the same time. So what happened was, it prepended it at first, but when the .append() method called it, it moved the element.
+
+// since a DOM element is unique, .prepend() and .append() can be used to move in addition to creating.
+
+// header.prepend(message); // move it back
+// header.append(message.cloneNode(true));
+// to have more than once instance, clone it. true means we clone child elements, too.
+
+// header.before(message); // moved before as sibling element
+// header.after(message); // moved after as sibling element
+
+// Recall also the .insertAdjacentHTML() method with position properties of beforebegin, afterbegin, beforeend, and afterend
+
+// HOW TO DELETE ELEMENTS
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // Old Way, DOM traversing (moving up and down in the DOM tree)
+    // message.parentElement.removeChild(message);
+
+    // New Way
+    message.remove();
+  });
