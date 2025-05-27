7f358825397f0c6d9e002339ea861866446f2fa2
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-05-27 19:46:30 -0400

    Create 205-DOM-Traversing.js

---
+++ JavaScript/The Complete JavaScript Course/205-DOM-Traversing.js
@@ -0,0 +1,32 @@
+/////////////////////////////////////////////////
+// 205-DOM-Traversing
+// Requires Bankist-DOM index.html and style.css files
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child elements
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parent elements
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling); // for nodes
+console.log(h1.nextSibling);
+
+// move to the parent and read all the children
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
