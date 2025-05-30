d8f05a69d9b694c6039ef2a82c21c4ab8513eb29
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-05-30 11:01:47 -0400

    Update script.js
    Added menu fade animation with event delegation and bind method.

--- All Projects/Bankist-DOM/script.js
+++ All Projects/Bankist-DOM/script.js
@@ -10,6 +10,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 ///////////////////////////////////////
 // Modal window
@@ -66,9 +70,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 // Common use case of event delegation: adding event handlers to buttons that do not exist at runtime but are created dynamically during page load.
 
 // Tabbed Component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 // Event delegation: add event handler to parent.
 tabsContainer.addEventListener('click', function (e) {
@@ -89,3 +90,20 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing an 'argument' into a handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
