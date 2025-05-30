4e3e93a13cd7913ea9be0c498ae7716fc95598ee
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-05-30 19:54:57 -0400

    Create 206-Tabbed-Component.js

---
+++ JavaScript/The Complete JavaScript Course/206-Tabbed-Component.js
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////
+// 206-Tabbed-Component
+// Requires Bankist-DOM index.html and style.css files
+
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// Event delegation: add event handler to parent.
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.toggle('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
