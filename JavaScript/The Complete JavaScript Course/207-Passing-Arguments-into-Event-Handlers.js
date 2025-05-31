fc49c292998f3516e785cb7ca5a287fcc51f8bbc
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-05-30 20:07:55 -0400

    Create 207-Passing-Arguments-into-Event-Handlers.js

---
+++ JavaScript/The Complete JavaScript Course/207-Passing-Arguments-into-Event-Handlers.js
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////
+// 207-Passing-Arguments-into-Event-Handlers
+// Requires Bankist-DOM index.html and style.css files
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing an "argument" into a handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
