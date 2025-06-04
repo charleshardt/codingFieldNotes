24198e2ecd87eeabf0ca542967ffdfd9a1f22217
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-06-04 10:34:11 -0400

    Create 210-Reveal-Elements-on-Scroll.js

---
+++ JavaScript/The Complete JavaScript Course/210-Reveal-Elements-on-Scroll.js
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////
+// 210-Reveal-Elements-on-Scroll
+// Requires Bankist-DOM index.html and style.css files
+
+// Reveal sections animation
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.25,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
