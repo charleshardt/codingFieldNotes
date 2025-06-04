28c02258d24e0a427a1135907231f70f96ba769e
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-06-04 12:27:47 -0400

    Create 211-Lazy-Loading-Images.js

---
+++ JavaScript/The Complete JavaScript Course/211-Lazy-Loading-Images.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////
+// 211-Lazy-Loading-Images
+// Requires Bankist-DOM index.html and style.css files
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
