a44d64a823cc6e0c02e75713168ec39de22733e3
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-06-10 09:53:31 -0400

    Create 219-Object-Oriented-Programming-OOP.js

---
+++ JavaScript/The Complete JavaScript Course/219-Object-Oriented-Programming-OOP.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////
+// 219-Object-Oriented-Programming-OOP
+
+OOP: Programming style, or paradigm, based on objects. 
+
+- Objects are used to model real-world (e.g. to-do list) or abstract (e.g. data structure) features.
+
+- Pack data (object properties) and corresponding behavior (methods) into one block.
+
+- Objects are self-contained blocks of code, which are used as building blocks of applications that interact with each other.
+
+- Interactions happen through a public interface--API.
+
+- OOP developed to organize code: make it more flexible, easier to maintain, less "spaghetti code."
+
+- Functional programming is an alternative to OOP.
+
+// Classes and Instances
+
+- Classes provide the ability to generate objects programmatically. They are like a blueprint. Here is a class blueprint to create a user object. It has properties and methods:
+
+Four Fundamental Principles to Good Class Implementation:
+ 1. Abstraction
+ 2. Encapsulation: making methods private within the class, preventing external code from manipulating internal properties/state (data). Encapsulation helps when changing code without breaking external code that relies on the object's methods.
+ 3. Inheritance: a child class inherits properties and methods from a parent class and extends them.
+ 4. Polymorphism: methods defined in a child class overwrite those inherited from a parent.
