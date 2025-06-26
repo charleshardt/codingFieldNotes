027000279db1a06aad6231d1057fe8e7bc47d270
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-06-26 14:48:19 -0400

    Create 243-Plan-a-Web-Project.js

---
+++ JavaScript/The Complete JavaScript Course/243-Plan-a-Web-Project.js
@@ -0,0 +1,48 @@
+/*
+/////////////////////////////////////////////////
+// 243-Plan-a-Web-Project
+
+PLANNING STEP
+
+1. USER STORIES. Description of the app's functionality from user's perspective. All user's stories combined describe th entire app.
+Common Format (who, what, why): As a [type of user], I want [an action] so that [a benefit].
+
+ Mapty: 
+
+ A. As a user, I want to log my running workouts with location, distance, time, pace, and steps/min, so I can keep a log of all my running.
+
+ B. As a user, I want to log my cycling workouts with location, distance, time, speed, and elevation gain, so I can keep a log of all my cycling.
+
+ C. As a user, I want to see all my workouts at a glance, so I can easily track my progress over time.
+
+ D. As a user, I want to see all my workouts on a map, so I can easily check where I work out the most.
+
+ E. As a user, I want to see all my workouts when I leave the app and come back later, so that I can keep using the app over time.
+
+2. FEATURES. All functionality needed to make the user stories happen.
+ 
+ A1. Map where user clicks to add a new workout (best way to get location coordinates)
+
+ A2. Geolocation to display map at current location (more user friendly)
+
+ A3. Form to input distance, time, pace, steps/min
+ 
+ B1. Form to input distance, time, speed, elevation gain
+
+ C1. Display workouts in a list
+ 
+ D1. Display workouts on a map
+
+ E1. Store workout in the browser using local storage API
+
+3. FLOWCHART. WHAT we build. Organization of the features into a visualization of all the actions a user can take and how the app reacts to the actions.
+
+ In the wild, you don't come up with a final flowchart right at the beginning. A rough sketch gets the ball rolling, and it develops throughout implementation. Over time, the more you do it, the more you can plan ahead.
+
+4. ARCHITECTURE. HOW we build it. What holds all the code together. How to organize the code, and which Js features to use.
+
+ Just like the flowchart, it develops over time. Start coding, and update the architecture as the complexity develops.
+
+DEVELOPMENT STEP. Implementation of planning steps 1-4.
+
+*/
