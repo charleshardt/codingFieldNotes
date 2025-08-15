c8a2ade9eb1b1ec7392e8485b7881f7134c3c397
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-08-15 10:19:25 -0400

    Update 271-Building-a-Simple-Promise.js

--- JavaScript/The Complete JavaScript Course/271-Building-a-Simple-Promise.js
+++ JavaScript/The Complete JavaScript Course/271-Building-a-Simple-Promise.js
@@ -3,12 +3,69 @@
 
 // New Promise with executor function. It must always end up in either the resolve state or the reject state.
 const lotteryPromise = new Promise(function (resolve, reject) {
-  if (Math.random() >= 0.5) {
-    resolve('You WIN 🍾');
-  } else {
-    reject('You lost your money 💩');
-  }
+  console.log('Lottery draw is happening 🔮');
+
+  // Encapsulate some async behaviour into this promise
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰💰🍾🍾');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 1000);
 });
 
 // .then() needs a callback for resolve state. .catch() will give error for reject state.
 lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// We usually only consume promises, but we use this pattern to wrap old callback-based functions into promises. That is called "promisifying": converting callback-based async behaviour to promise-based.
+
+// Promisifying setTimeout(). Create a function and return a promise, thereby encapsulating the async behaviour. That is essentially what fetch() does.
+
+// const wait = function (seconds) {
+//   // no reject needed since setTimeout cannot fail:
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+const wait = seconds =>
+  new Promise(resolve => setTimeout(resolve, seconds * 1000));
+
+wait(1)
+  .then(() => {
+    console.log('I waited for 1 seconds.');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for another second.');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 3 seconds.');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for one last second.'));
+
+// That chain is way better than this callback hell:
+
+setTimeout(() => {
+  console.log('\t\tJust over 1 second has passed');
+  setTimeout(() => {
+    console.log('\t\tJust over how many? seconds passing passed will pass');
+    setTimeout(() => {
+      console.log('\t\tJust over 3? time units? passing rhzhfff');
+      setTimeout(() => {
+        console.log(
+          '\t\tToo many seconds passed, so messy, so buggy, mrffzxqchhhk-k-k-k-k...'
+        );
+      }, 1001);
+    }, 1001);
+  }, 1001);
+}, 1001);
+
+// Make a fulfilled or rejected promise immediately
+
+// A static method on the Promise constructor and a handler:
+Promise.resolve('Resolved value').then(x => console.log(x));
+Promise.reject(new Error('Rejected value')).catch(x => console.error(x));
