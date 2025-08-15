d22d1b48ce0125ef47dc6607672fa13f983084fd
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2025-08-15 12:01:27 -0400

    Create 272-Promisifying-the-Geolocation-API.js

---
+++ JavaScript/The Complete JavaScript Course/272-Promisifying-the-Geolocation-API.js
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////
+// 272-Promisifying-the-Geolocation-API
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+  <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row">
+    <span>👫</span>${(+data.population / 1000000).toFixed(1)}
+    </p>
+    <p class="country__row">
+    <span>🗣️</span>${data.languages.eng}
+    </p>
+    <p class="country__row">
+    <span>💰</span>${Object.values(data.currencies)[0].name}
+    </p>
+    </div>
+    </article>
+    `;
+  // const html = 'hellow world';
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`
+      );
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      const stateCountry =
+        data.countryCode === 'US'
+          ? data.principalSubdivision
+          : data.countryName;
+      console.log(`You are in ${data.city}, ${stateCountry}.`);
+      return fetch(`https://restcountries.com/v3.1/alpha/${data.countryCode}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country cannot be found.(${res.status})`);
+      return res.json();
+    })
+    .then(data => {
+      renderCountry(data[0]);
+    })
+    .catch(err => {
+      console.error(`💥 ${err.message} 💥`);
+    });
+};
+
+btn.addEventListener('click', whereAmI);
