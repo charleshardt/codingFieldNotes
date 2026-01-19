17604b3e0a0e0553ce506fa8cc1691d8d99efdba
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2026-01-19 17:44:35 -0500

    Refactor advice state and add count functionality

--- React/The Ultimate React Course/002-our-first-react-app.js
+++ React/The Ultimate React Course/002-our-first-react-app.js
@@ -1,20 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+// The most fundamental concept in React.js is state.
 
 export default function App() {
-  const [advice, setAdvice] = useState("Help me help you");
+  const [advice, setAdvice] = useState("");
 
+  const [count, setCount] = useState(0);
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
 
   return (
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Help yourself</button>
+      <Message count={count} />
     </div>
   );
 }
 
-// state is the most fundamental concept in React
+// We need to divide user interfaces into components.
+// Uppercase, React convention for components.
+function Message(props) {
+  return (
+    <p>
+      I have helped you <strong>{props.count}</strong> times.
+    </p>
+  );
+}
