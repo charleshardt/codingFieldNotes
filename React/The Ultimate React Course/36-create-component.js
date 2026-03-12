db74bc4a6d0fe6949279b6a81ee9999576e9cb9e
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2026-03-12 13:26:55 -0400

    Create create-component.js
    Created a simple React component structure with a Rabbit component.

---
+++ React/The Ultimate React Course/36-create-component.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, Rpbdb? /'Ri-pi-bi-dib/</h1>
+      <Rabbit />
+    </div>
+  );
+}
+
+// Creating a component
+// 1. Uppercase
+// 2. Contain markup: jsx or null
+
+function Rabbit() {
+  return <h2>No! That spells /ra-bit/!</h2>;
+}
