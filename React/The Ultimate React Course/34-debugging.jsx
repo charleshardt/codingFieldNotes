77f7537b700ff257f5ce7eea43934a19bbe3013b
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2026-03-12 12:23:03 -0400

    Create 34-debugging.jsx
    Added tips for debugging React applications.

---
+++ React/The Ultimate React Course/34-debugging.jsx
@@ -0,0 +1,10 @@
+/**
+ * Tips for debugging
+ *
+ * 1. Reload page
+ * 2. ^C Stop terminal re-run 'npm start'
+ * 3. Always keep the terminal open on VS Code and Dev Tools open on the browser.
+ * 4. Create React App error overlay: grab error message and google.
+ * 5. Always work with ESLint, observe the underlined code and the Problems tab.
+ * 6. Keep an eye on the Output tab for ESLint and Prettier.
+ */
