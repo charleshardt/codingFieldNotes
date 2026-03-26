b445b935a13f8969ffe8f6290c839fdb972765d3
Charles Hardt <67312092+charleshardt@users.noreply.github.com>
2026-03-26 10:10:40 -0400

    Change import from App to 57-steps-App

--- React/The Ultimate React Course/57-steps-index.js
+++ React/The Ultimate React Course/57-steps-index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./57-steps-index.css";
-import App from "./App";
+import App from "./57-steps-App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
